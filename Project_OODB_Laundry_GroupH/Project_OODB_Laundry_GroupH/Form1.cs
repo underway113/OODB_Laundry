b5502ded301862dd09409905789991e653ea707c
underway113 <jeradam123@yahoo.com>
2017-03-23 21:01:30 +0700

    Init Commit + FormLogin(basic)

---
+++ Project_OODB_Laundry_GroupH/Project_OODB_Laundry_GroupH/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_OODB_Laundry_GroupH
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+            //COMMENT INIT INIT INIT
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
